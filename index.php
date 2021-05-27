934a252ca18069c0e8c815178c9b2cafa82cbd89
javierfree <60582436+javierfree@users.noreply.github.com>
2021-05-27 09:05:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html>
+		<head>
+				<meta charset="utf-8">
+				<meta name="description" content="facturacion electronica, sistema de facturacion, stock, ventas">
+				<meta name="keywords" content="facturacion electronica, sistemas, desarrollos, soporte tecnico">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link rel="shortcut icon" href="img/favicon.png">
+				<link rel = "preconnect" href = "https://fonts.gstatic.com">
+				<link href = "https://fonts.googleapis.com/css2? family = Roboto: wght @ 100; 300; 400; 500; 700; 900 & display = swap "rel =" hoja de estilo ">
+				<link rel="stylesheet"  href="css/normalize.css">
+				<link rel="stylesheet"  href="style.css">
+				<script src="https://kit.fontawesome.com/42fd5ef1e8.js" crossorigin="anonymous"></script>
+				<title>
+						Inicio-Javier Castillo
+				</title>
+		</head>
+		<body>
+				<header>
+						<div class="container">
+								<a href="index.php">
+									<img src="img/favicon.png" class="logo">					
+								</a>
+								<nav>
+									<a href="#inicio"><i class="fas fa-home"></i> Inicio</a>
+									<a href="#nosotros"><i class="fas fa-users"></i> Nosotros</a>
+									<a href="#servicios"><i class="fas fa-tools"></i> Servicios</a>
+									<!-- <a href="#galeria">Galeria</a> -->
+									<a href="#contactenos"><i class="far fa-address-card"></i> Contáctenos</a>
+								</nav>
+								<a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+						</div>					
+				</header>
+				<main>
+						<section id="inicio">
+							<img src="img/bannerprincipal2.jpg">
+							<div class="bloque-inicio">		
+								<h1>Software para Puntos de Venta</h1>
+								<!-- <p>
+									Sistemas para Facturación Electrónica, Control de Stock, Manejo de Cajas, Cuentas Corrientes De Clientes. Soporte Técnico, Capacitación Online, Precios A Medida.
+								</p> -->
+								<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+							</div>
+						</section>
+						<section id="nosotros" class="seccion">
+							<div class="container">
+								<!-- <p><b>Venta y Desarrollo De Sistemas Para Facturación Electrónica y Stock.</b></p> -->
+								<h2>Venta y Desarrollo De Sistemas Para Facturación Electrónica y Stock.</h2>
+							</div>
+						</section>
+						<section id="servicios" class="seccion">
+							<div class="container">
+								<div class="row">
+									<div class="columna columna-25 columna-mobile-100">
+										<div class="bloque-servicio">
+											<div class="bloque-img-servicio cuadrado-perfecto">
+												<img src="img/servicio1.jpg">
+											</div>
+											<div class="bloque-contenido-servicio">
+												<h3>
+													Funciones Básicas					
+												</h3>
+												<p>Facturación electrónica, cambios de precios por rubros, marcas, control de cajas, reportes de movimientos, impresion sin controlador Fiscal.</p>
+												<a href="#contactenos" class="boton boton-blanco">Contacto</a>												
+											</div>
+										</div>										
+									</div>
+									<div class="columna columna-25 columna-mobile-100">
+										<div class="bloque-servicio">
+											<div class="bloque-img-servicio cuadrado-perfecto">
+												<img src="img/servicio2.jpg">
+											</div>
+											<div class="bloque-contenido-servicio">
+												<h3>
+													Abono Mensual			
+												</h3>
+												<p>Con esta Opción, pagas un abono mensual $ 2500, que te va incluir soporte remoto, actualizaciones, mesa de ayuda</p>
+												<a href="#contactenos" class="boton boton-blanco">Contacto</a>												
+											</div>
+										</div>
+									</div>
+									<div class="columna columna-25 columna-mobile-100">
+										<div class="bloque-servicio">
+											<div class="bloque-img-servicio cuadrado-perfecto">
+												<img src="img/servicio3.jpg">
+											</div>
+											<div class="bloque-contenido-servicio">
+												<h3>
+													Compra					
+												</h3>
+												<p>Si compras, tenés instalación sin cargo, capacitación online por 30 dias. Se cobran las actualizaciones y soporte.</p>
+												<a href="#contactenos" class="boton boton-blanco">Contacto</a>												
+											</div>
+										</div>
+									</div>	
+
+
+											<div class="columna columna-25 columna-mobile-100">
+										<div class="bloque-servicio">
+											<div class="bloque-img-servicio cuadrado-perfecto">
+												<img src="img/serviciosinformaticos.jpg">
+											</div>
+											<div class="bloque-contenido-servicio">
+												<h3>
+													Otros Servicios				
+												</h3>
+												<p>Soporte a sistemas BASCS y QUICKPOS. Adaptaciones e integraciones con otros sistemas. Control y manejo de errores</p>
+												<a href="#contactenos" class="boton boton-blanco">Contacto</a>												
+											</div>
+										</div>
+									</div>
+
+
+								</div>								
+							</div>
+						</section>
+						<!-- <section id="galeria">
+								<div class="container-fluid">
+									<div class="row">
+										<div class="columna-33 columna-mobile-50">
+											<div class="cuadrado-perfecto">
+												<img src="img/gale1.jpg">
+												<h4>
+													Recorcholis
+												</h4>												
+											</div>												
+										</div>
+										<div class="columna-33 columna-mobile-50">	
+											<div class="cuadrado-perfecto">
+												<img src="img/gale2.jpg">
+												<h4>
+													Nuevo Siglo
+												</h4>												
+											</div>												
+										</div>
+										<div class="columna-33 columna-mobile-50">
+											<div class="cuadrado-perfecto">
+												<img src="img/gale3.jpg">
+												<h4>
+													YPF Francisco De Aguirre
+												</h4>												
+											</div>												
+										</div>
+										<div class="columna-33 columna-mobile-50">
+											<div class="cuadrado-perfecto">
+												<img src="img/gale4.jpg">
+												<h4>
+													La Paneria YB - Sucursales
+												</h4>												
+											</div>												
+										</div>
+										<div class="columna-33 columna-mobile-50">	
+											<div class="cuadrado-perfecto">
+												<img src="img/gale5.jpg">
+												<h4>
+													Panificación Albertus
+												</h4>												
+											</div>												
+										</div>
+										<div class="columna-33 columna-mobile-50">
+											<div class="cuadrado-perfecto">
+												<img src="img/gale6.jpg">
+												<h4>
+													Carmela Resto YB
+												</h4>												
+											</div>												
+										</div>
+
+										
+
+
+
+									</div>
+								</div>
+						</section> -->
+
+						
+
+						<section id="contactenos" class="seccion">
+
+							<!-- <iframe width="520" height="400" frameborder="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=catamarca%20328%20yerba%20buena+(Mi%20Casa)&amp;t=h&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">								
+							</iframe> -->
+
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56973.348569491514!2d-65.33037766296349!3d-26.813404687635273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9422433a2ee1bffb%3A0xc5d4083eee0d98b1!2sYerba%20Buena%2C%20Tucum%C3%A1n!5e0!3m2!1ses-419!2sar!4v1622110479382!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+							<div class="container-fluid">
+								<div class="row">
+									<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+										<form action="index.php" method="post">
+											<div class="form-block">
+												<input type="text" name="nombre" class="form-control" placeholder="Nombre">								
+											</div>
+											<div class="form-block">
+												<input type="email" name="email" class="form-control" placeholder="Email">		
+											</div>
+											<div class="form-block">
+												<textarea name="mensaje" placeholder=""></textarea>
+											</div>
+											<div class="form-block bloque-ultimo">
+												<input type="submit" class="boton boton-negro" value="Enviar">
+											</div>								
+											<?php 
+
+												if($_SERVER["REQUEST_METHOD"] == "POST")
+												{
+													$nombre = $_POST["nombre"] ;
+													$email = $_POST["email"] ;
+													$mensaje = $_POST["mensaje"] ;
+
+													if(isset($nombre))
+													{
+														if(isset($email))
+														{
+															if (isset($mensaje))
+															{
+
+																$para = "sistemasamedida010101@gmail.com";
+																$asunto = "Contacto Web";
+																$cuerpo = $nombre."\n".$email."\n".$mensaje;
+																$adicional = "from:jjaviercastillo@outlook.com";
+																mail($para,$asunto,$cuerpo,$adicional);
+											?>
+
+											<p>Envio Exitoso</p>
+
+											<?php
+
+															}
+
+														}
+													}
+													
+												}
+
+											?>									
+										</form>										
+									</div>									
+								</div>
+							</div>	
+
+							
+						</section>
+				</main>
+				<footer>
+					<div class="container">
+							<div class="row">
+								<div class="columna columna-33 columna-mobile-100">
+									<img src="img/logo-blanco.png" class="logo-footer">
+									<p>
+										<!-- algo bla bla  -->
+									</p>									
+								</div>
+								<!-- <div class="columna columna-25 columna-mobile-100">
+									<!-- <h3>
+										Temas Relacionados
+									</h3> -->
+									<!-- <ul>
+										<li><a href="#inicio">Inicio</a></li>
+										<li><a href="#nosotros">Nosotros</a></li>
+										<li><a href="#servicios">Servicios</a></li>	
+									</ul>		 -->							
+								<!-- </div> --> 
+								<div class="columna columna-33 columna-mobile-100">
+									<h3>
+										Datos De Contacto
+									</h3>
+									<ul>
+										<li><i class="far fa-envelope"></i> sistemasamedida010101@gmail.com</li>
+										<li><i class="fab fa-whatsapp"></i> 3815420834</li>
+										<li>Yerba Buena - Tucuman</li>
+									</ul>									
+								</div>
+								<div class="columna columna-33 columna-mobile-100">
+									<h3>
+										Redes Sociales
+									</h3>
+									<ul class="redes">
+										<li>
+											<a href="https://www.instagram.com/programasamedida/"target="_blank">
+												<i class="fab fa-instagram-square">
+												</i>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.facebook.com/programas.amedida.12/"target="_blank">
+												<i class="fab fa-facebook-square">		
+												</i>
+											</a>
+										</li>
+										<!-- <li>
+											<a href="#">
+												<i class="fab fa-twitter-square">	
+												</i>
+											</a>
+										</li> -->
+									</ul>									
+								</div>								
+							</div>
+					</div>
+					
+					<div class="barra-footer">
+						&copy; Derechos Reservados - Sistemas a Medida - 2021						
+					</div>
+				</footer>
+				<script src="js/jquery.js"></script>
+				<script src="js/funciones.js"></script>
+
+		</body>
+</html>
